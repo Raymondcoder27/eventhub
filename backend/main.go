da3aad5166e171612626b0b260d94bdd4c4e464f
MwebeCoder27 <mwebraymond13@gmail.com>
2024-09-12 15:46:44 +0300

    init

--- backend/main.go
+++ backend/main.go
@@ -10,9 +10,9 @@ func init() {
 
 func main() {
 	r := gin.Default()
-	r.POST()
-	r.GET()
-	r.PUT()
-	r.DELETE()
+	r.POST("/createEvent", controllers.CreateEvent)
+	r.GET("/events", controllers.EventsIndex)
+	r.PUT("/events", controllers.UpdateEvent)
+	r.DELETE("/event/:id", controllers.DeleteEvent)
 	r.Run()
 }

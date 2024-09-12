21abd6fdfe6cb6bebfb707f132e3b90220b7369c
MwebeCoder27 <mwebraymond13@gmail.com>
2024-09-12 16:13:15 +0300

    init

--- backend/controllers/eventController.go
+++ backend/controllers/eventController.go
@@ -1,6 +1,14 @@
 package controllers
 
-// getting all events
-func EventsIndex() {
+import (
+	"net/http"
 
+	"github.com/eventhub/models"
+	"github.com/gin-gonic/gin"
+)
+
+// getting all events
+func EventsIndex(c *gin.Context) {
+	var events []models.Event
+	c.JSON(http.StatusOK, gin.H{"message": events})
 }

0ec97cc5d36263567413c2ed389ab631a5f4c2af
MwebeCoder27 <mwebraymond13@gmail.com>
2024-09-12 15:52:16 +0300

    init commit

--- backend/initializers/database.go
+++ backend/initializers/database.go
@@ -1,10 +1,17 @@
 package initializers
 
-import "gorm.io/gorm"
+import (
+	"os"
+
+	"gorm.io/gorm"
+)
 
 var DB *gorm.DB
 
 func ConnectToDB() {
 	var err error
+	dbName := os.Getenv("DB_NAME")
+	dbHost := os.Getenv("DB_HOST")
+	dbPassword := os.Getenv("DB_PASSWORD")
 	dsn := 
 }

aa0bdf1cb11b014eec11f89aaaa0daef0f1fce28
MwebeCoder27 <mwebraymond13@gmail.com>
2024-09-12 15:44:05 +0300

    init commit

--- backend/main.go
+++ backend/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "github.com/gin-gonic/gin"
+
 func init() {
 	initializers.LoadEnvVars()
 	initializers.ConnectToDB()
@@ -8,4 +10,9 @@ func init() {
 
 func main() {
 	r := gin.Default()
+	r.POST()
+	r.GET()
+	r.PUT()
+	r.DELETE()
+	r.Run()
 }
